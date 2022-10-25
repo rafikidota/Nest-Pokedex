c1d23cc60f6b8ecf68b17c74db6c78e1726e7855
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-10-25 04:32:42 -0400

    https

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
