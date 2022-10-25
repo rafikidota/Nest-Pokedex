8efae0228df447929bb7fa3d11145c988083fdbf
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-10-25 17:12:35 -0400

    global prefix

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
   });
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
