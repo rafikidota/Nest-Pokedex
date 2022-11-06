393821b343966761b5cf4c8443bcd72f0a09b5a0
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-06 16:25:47 -0500

    set up global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as fs from 'node:fs';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const httpsOptions = {
@@ -11,6 +12,17 @@ async function bootstrap() {
     httpsOptions,
   });
   app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  );
   await app.listen(3000);
 }
 bootstrap();
