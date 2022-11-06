d6efbdfc5c9cbb5a2ceb7c189a6b2a1158453f89
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-05 21:35:05 -0400

    get pokemons from pokeapi.co

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { IPokemon } from './interfaces/IPokemon.interface';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
+  private url = 'https://pokeapi.co/api/v2/pokemon?limit=500';
+  private readonly axios: AxiosInstance = axios;
 
-  execute_seed() {
-    
-    return `seed executed`;
+  async execute_seed() {
+    const { data } = await this.axios.get<PokeResponse>(this.url);
+    const pokemons: IPokemon[] = [];
+    data.results.forEach(({ name, url }) => {
+      const pokemon: IPokemon = {
+        name,
+        id_number: this.get_id_number(url)
+      }
+      pokemons.push(pokemon);
+    });
+    return pokemons;
+  }
+
+  private get_id_number(url: string) {
+    const segments = url.split('/');
+    const id_number = +segments[segments.length - 2];
+    return id_number;
   }
 
 }
