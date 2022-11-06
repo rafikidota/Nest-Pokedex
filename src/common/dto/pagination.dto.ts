05c2763fe2babfc20ce070e77d1f7e853c3457f1
rafiki <71207415+rafikidota@users.noreply.github.com>
2022-11-06 16:26:03 -0500

    Create pagination.dto.ts

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,17 @@
+import { IsInt, IsNumber, IsOptional, IsPositive, Min } from "class-validator";
+
+export class PaginationDto {
+    @IsOptional()
+    @IsNumber()
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    limit: number;
+    
+    @IsOptional()
+    @IsNumber()
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    offset: number;
+}
